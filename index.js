f061b0838aae07661b12c55ee8c9aca2c114598e
Yuna <una3917@naver.com>
2025-12-02 11:03:43 +0900

    2025-12-02

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
